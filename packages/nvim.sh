c7a385179b3b705fd08e2cc231b9264aca17e950
dmitrylito <dmitrylitoshik15@gmail.com>
2025-11-27 02:00:34 -0500

    Dot files updated

--- packages/nvim.sh
+++ packages/nvim.sh
@@ -2,66 +2,80 @@
 
 set -xe
 
-# sh ./fonts.sh
-
-NVIM_VERSION=v0.10.0
+# Set to "stable" to always get the latest release (currently > 0.10.2)
+NVIM_VERSION=stable
 
 # Check if I'm connected as root user. If not, exit.
 if [ "$(id -u)" != "0" ]; then
-	echo "Sorry, you are not root."
-	exit 1
+  echo "Sorry, you are not root."
+  exit 1
 fi
 
-# Updating libraries to ensure nodejs >= 14 is being installed.
+# Updating libraries
 cd "${HOME}"
 
 # Installing neovim dependencies
 apt-get update
 apt-get install -y \
-	git \
-	curl \
-	zip \
-	libluajit-5.1-dev \
-	ripgrep \
-	fd-find
+  git \
+  curl \
+  zip \
+  libluajit-5.1-dev \
+  ripgrep \
+  fd-find
 
-# Installing lazy git
+# Installing lazygit (Dynamic latest version fetch)
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 install lazygit /usr/local/bin
 rm lazygit.tar.gz lazygit
 
-# .deb is not supported for 0.9.0 version upwards. More info: https://github.com/neovim/neovim/issues/22684
-# Another options for installing neovim:
-# https://github.com/MordechaiHadad/bob
+# ------------------------------------------------------
+# NEOVIM INSTALLATION
+# ------------------------------------------------------
 
-# Install neovim in the way above only for ARM architectures
+# 1. ARM / AARCH64 (Build from source)
 if [ "$(uname -m)" = "aarch64" ] || [ "$(uname -m)" = "arm" ]; then
-	# Steps defined here: https://dev.to/asyncedd/building-neovim-from-source-1794
-	apt-get update
-	apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
-	git clone https://github.com/neovim/neovim.git
-	# Versions higher than v0.8.3 present visualization problems when connected inside neovim
-	cd neovim
-	git checkout ${NVIM_VERSION}
-	make CMAKE_BUILD_TYPE=RelWithDebInfo
-	make install
-	cd -
-	rm -rf neovim
+  echo "Detected ARM architecture. Building from source..."
+
+  # Install build prerequisites
+  apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+
+  # Clone and build
+  rm -rf neovim # Clean previous builds if any
+  git clone https://github.com/neovim/neovim.git
+  cd neovim
+  git checkout ${NVIM_VERSION}
+  make CMAKE_BUILD_TYPE=RelWithDebInfo
+  make install
+  cd -
+  rm -rf neovim
+
+# 2. X86_64 (AppImage)
 else
-	# Installing neovim via appimage. Recommended approach: https://github.com/neovim/neovim/releases/download/v0.9.1/nvim-linux64.deb
-	# curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+  echo "Detected x86_64 architecture. Installing via AppImage..."
 
-	curl -LO https://github.com/neovim/neovim/releases/download/${NVIM_VERSION}/nvim.appimage
-	chmod u+x nvim.appimage
-	./nvim.appimage --appimage-extract
-	./squashfs-root/AppRun --version
-	rm nvim.appimage
+  # Download the release defined by NVIM_VERSION (stable)
+  curl -LO "https://github.com/neovim/neovim/releases/download/${NVIM_VERSION}/nvim.appimage"
+  chmod u+x nvim.appimage
 
-	# Exposing nvim globally
-	if [ ! -d /squashfs-root ]; then
-		mv squashfs-root /
-	fi
-	ln -s /squashfs-root/AppRun /usr/bin/nvim
+  # Extract
+  ./nvim.appimage --appimage-extract
+
+  # Verify it runs
+  ./squashfs-root/AppRun --version
+
+  rm nvim.appimage
+
+  # Exposing nvim globally
+  # Remove existing installation to avoid conflicts
+  rm -rf /squashfs-root
+  mv squashfs-root /
+
+  # Link binary (overwrite if exists)
+  ln -sf /squashfs-root/AppRun /usr/bin/nvim
 fi
+
+echo "Neovim installation complete."
+nvim --version
